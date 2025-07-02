5b3850661b7a316da66f13dfc4dd71f3f0c3f814
sriramsingh <sriramsingh084@gmail.com>
2025-07-02 19:16:23 +0530

    a+b

--- src/java_8_features/java_eight.java
+++ src/java_8_features/java_eight.java
@@ -1,4 +1,15 @@
 package java_8_features;
 
 public class java_eight {
+
+
+    public static void main(String[] args) {
+        int a= 3;
+        int b = 5;
+        int c=a+b;
+        System.out.println(c);
+    }
+
+
+
 }
