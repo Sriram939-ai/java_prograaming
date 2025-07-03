55ba67132880187d398f13da5edf48c044d295a3
sriramsingh <sriramsingh084@gmail.com>
2025-07-03 12:17:48 +0530

    reverse and palindrom prograam

---
+++ reverse_string.java
@@ -0,0 +1,42 @@
+
+public class reverse_string {
+
+    
+
+
+    public static void main(String[] args) {
+
+
+
+     String s = "shubham sriram singh";
+
+
+
+     char[] ch =s.toCharArray();
+
+
+
+     String rev = "";
+
+
+
+     for (int i = ch.length-1; i>=0;   i--){
+
+
+
+        rev = rev + ch[i];
+
+     }
+
+     System.out.println(rev);
+
+
+
+
+
+    }
+
+
+
+
+}
