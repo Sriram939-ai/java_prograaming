296851ad39d0828aae51ba51490b549394d3c07d
sriramsingh <sriramsingh084@gmail.com>
2026-07-09 15:37:05 +0530

    xyzx

--- src/Demo.java
+++ src/Demo.java
@@ -5,7 +5,7 @@ Demo(){
 }
 
 void display(){
-    System.out.println("Display my msg");
+    System.out.println("Display my msg i am sriram singh");
 }
     public static void main(String[] args) {
         System.out.println("Welcome");
